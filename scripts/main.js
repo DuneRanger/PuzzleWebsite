71abf481494e238b858f056e2c8a21eca15a1fa2
Daniel Culliver <80460672+DuneRanger@users.noreply.github.com>
2021-09-19 21:44:24 +0200

    eyyyyyy converted it to p5. Now I just gotta splice the image by pixels...

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,38 @@
 let files = ["Anomaly.jpg","Arctic fox.png","Astroworld.png","Ayaka.png","Cloud beast.png","Detailed1.jpg","Detailed2.jpg","Detailed3.jpg","Fallen Titan.png","Fireflies.jpg","Forest temple.jpg","Godwhale.jpg","Goldenwalk.jpg","Jungle Harbour.jpg","life death.jpg","lonely cruise.jpg","Misty forest.jpg","Mushroom Tree.jpg","Neon Samurai.jpg","North pole.jpg","Odyssey.jpg","Paranoid.jpg","Planet V_02.jpg","Portal.jpg","Portal.png","RDR2.jpg","Relaxing life.jpg","River side.jpg","Shores of Space.jpg","Soaring Roots.jpg","Supertree city.jpg","The horsemen.png","The journey of Elaina.jpg","The Mystery.jpg","Tree of Life.jpg","Twinflame.png","Void under rule.jpg","Winter wolf.png","Winter.png","Yaksha.png"];
-let filesTEST = ["Anomaly.jpg","Detailed1.jpg","The horsemen.png","Void under rule.jpg"];
+let filesTEST = ["Anomaly.jpg","Detailed1.jpg","The horsemen.png","Godwhale.jpg"];
+let Canvas, img;
 
-const newPuzzle = document.getElementById("newPuzzle");
-const main = document.getElementById("main");
-const disclaimer = document.getElementById("disclaimer");
-const puzzle = document.getElementById("puzzle");
-disclaimer.setAttribute("textContent", "NOTE: I do not own any of these images and this site is not intended for commercial use");
-puzzle.setAttribute("alt", "An error has probably occured whilst loading this image");
+function setup() {
+    Canvas = createCanvas(innerWidth*0.8, innerHeight*0.8);
+    img = loadImage(getRandomImage(files))
+    resizeImgCanvas(img)
+    imageMode(CORNERS)
+    Canvas.parent("canvas")
+}
+
+function draw() {
+    fill(200)
+    rect(0, 0, Canvas.width, Canvas.height)
+    image(img, 0, 0, Canvas.width, Canvas.height)
+}
 
 function getRandomImage(src) { 
     let randomInd = Math.ceil(Math.random()*src.length-1);
     return "./images/puzzlePics/" + src[randomInd];
 }
 
-
-function resizeImage(img) {
-    let test = new Image();
-    test.onload = function() {
-        let newW, newH;
-        img.width = test.width;
-        img.height = test.height;
-
-        do {
-            // Calculates ratio of image to window width | height and adjusts resolution accordingly
-            newW = innerWidth/img.width *0.8;
-            newH = innerHeight/img.height *0.8;
-            puzzle.setAttribute("height", img.height * Math.min(newW, newH));
-            puzzle.setAttribute("width", img.width * Math.min(newW, newH));
-        } while (img.width > innerWidth || img.height > innerHeight);
+function resizeImgCanvas(img) {
+    img.height *= 1.5
+    img.width *= 1.5
+    while(img.height > innerHeight*0.85 || img.width > innerWidth*0.8) {
+        img.width *= 0.9
+        img.height *= 0.9
     }
-    test.src = img.src;
+    resizeCanvas(img.width, img.height)
 }
 
-
 newPuzzle.onclick = function() {
-    puzzle.setAttribute('src', getRandomImage(files));
-    resizeImage(puzzle);
+    img = loadImage(getRandomImage(files), img => {
+        resizeImgCanvas(img)
+    })
 }
