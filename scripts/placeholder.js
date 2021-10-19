026cad332892af9f146d5fde63a0d028a5163dc7
Daniel Culliver <80460672+DuneRanger@users.noreply.github.com>
2021-10-19 17:36:20 +0200

    Cleanup

--- scripts/placeholder.js
+++ scripts/placeholder.js
@@ -1,3 +1,8 @@
+//Man, is there really not a better solution than this for local machines?
+//I know I can improve this if I put it on a server, but at that point, I might as well put the images on the server as well, no? 
+//And that would make this redundant...
+
+
 const fs = require("fs")
 let files = fs.readdirSync("./images")
 console.log(JSON.stringify(files))
